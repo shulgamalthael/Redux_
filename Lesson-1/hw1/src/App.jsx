0328e234942bafd0e37e160ce21474b54a09e4dc
Shulha Nikita <shulgamalthael@gmail.com>
2020-11-09 12:55:40 +0200

    init files

--- Lesson-1/hw1/src/App.jsx
+++ Lesson-1/hw1/src/App.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
+import { UserDataContext } from './context'
+
 import Header from './Header';
-import { UserDataContext } from './context';
 
 class App extends React.Component {
-
-    state = {
-        userData: { 
-            name: 'Nikola Tesla', 
-            avatar_url: 'https://avatars3.githubusercontent.com/u10001' 
-        }
+  state = {
+    userData: { 
+      name: 'Nikola Tesla', 
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001' 
     }
+  }
 
-    render() {
-        return(
-            <div class="page">
-                <UserDataContext.Provider value={this.state.userData}>
-                    <Header/>
-                </UserDataContext.Provider>
-            </div>
-
-        )
-    }
-}
+  render() {
+    return (
+      <div className="page">
+        <UserDataContext.Provider value={this.state.userData} >
+          <Header />
+        </UserDataContext.Provider>
+      </div>
+    )
+  }
+};
 
 export default App;
