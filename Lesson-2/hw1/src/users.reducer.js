d419c71132c018e819b1a399d41e6c80c4a3dd56
Shulha Nikita <shulgamalthael@gmail.com>
2020-11-09 18:26:57 +0200

    mod

--- Lesson-2/hw1/src/users.reducer.js
+++ Lesson-2/hw1/src/users.reducer.js
@@ -1,26 +1,26 @@
-import { ADD_USER, DELETE_USER } from './users.actions'
+import { ADD_USER, DELETE_USER} from './users.actions';
 
-export const initialState = {
-    usersList: []
+const initialState = {
+  usersList: []
 }
 
 export const reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case ADD_USER:
-        return {
-          ...state,
-          usersList: [
-            ...state.usersList,
-            action.userData
-          ]
-        }
-      case DELETE_USER:
-  
-        return {
-          ...state,
-          usersList: state.usersList.filter(({id}) => id !== action.id)
-        }
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case ADD_USER:
+      return {
+        ...state,
+        usersList: [
+          ...state.usersList,
+          action.userData
+        ]
+      }
+    case DELETE_USER:
+
+      return {
+        ...state,
+        usersList: state.usersList.filter(({id}) => id !== action.id)
+      }
+    default:
+      return state;
+  }
 }
