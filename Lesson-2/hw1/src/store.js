934b2c32fb9d1f04a9f656a2df858349e6248f52
Shulha Nikita <shulgamalthael@gmail.com>
2020-11-09 18:26:50 +0200

    init files

--- Lesson-2/hw1/src/store.js
+++ Lesson-2/hw1/src/store.js
@@ -3,4 +3,4 @@ import { reducer } from './users.reducer';
 
 const store = createStore(reducer);
 
-export default store;
+export default store
--- Lesson-2/hw1/src/users.actions.js
+++ Lesson-2/hw1/src/users.actions.js
@@ -1,14 +1,16 @@
 export const ADD_USER = 'USERS/ADD_USER';
 export const DELETE_USER = 'USERS/DELETE_USER';
 
-export const addUser = () => {
-    return {
-        type: ADD_USER
-    }
+export const addUser = userData => {
+  return {
+    type: ADD_USER,
+    userData
+  }
 }
 
-export const deleteUser = () => {
-    return {
-        type: DELETE_USER
-    }
+export const deleteUser = id => {
+  return {
+    type: DELETE_USER,
+    id
+  }
 }
