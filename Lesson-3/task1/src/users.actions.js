6bdd8685e0cf3b3fa07707020387a159a0db0580
Shulha Nikita <shulgamalthael@gmail.com>
2020-11-12 15:36:11 +0200

    init files

--- Lesson-3/task1/src/users.actions.js
+++ Lesson-3/task1/src/users.actions.js
@@ -2,7 +2,7 @@ export const ADD_USER = 'USERS/ADD_USER';
 export const DELETE_USER = 'USERS/DELETE_USER';
 export const UPDATE_USER = 'USERS/UPDATE_USER';
 
-export const user_add = (userData) => {
+export const add_user = (userData) => {
     return {
         type: ADD_USER,
         payload: {
@@ -11,7 +11,7 @@ export const user_add = (userData) => {
     }
 }
 
-export const user_delete = (userId) => {
+export const delete_user = (userId) => {
     return {
         type: DELETE_USER,
         payload: {
@@ -20,7 +20,7 @@ export const user_delete = (userId) => {
     }
 }
 
-export const user_update = (userId, userData) => {
+export const update_user = (userId, userData) => {
     return {
         type: UPDATE_USER,
         payload: {
