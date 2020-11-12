dca2635d998f51025eea8b2b085fdbaedf06e842
Shulha Nikita <shulgamalthael@gmail.com>
2020-11-12 17:18:12 +0200

    init files

--- Lesson-3/hw1/src/cart.action.js
+++ Lesson-3/hw1/src/cart.action.js
@@ -1,20 +1,20 @@
-export const ADD_PRODUCT = 'PRODUCTS/ADD_PRODUCT';
-export const DELETE_PRODUCT = 'PRODUCTS/DELETE_PRODUCT';
+export const ADD_PRODUCT = 'CART/ADD_PRODUCT';
+export const REMOVE_PRODUCT = 'CART/REMOVE_PRODUCT';
 
-export const add_product = (productData) => {
-    return {
-        type: ADD_PRODUCT,
-        payload: {
-            productData,
-        }
+export const addProduct = productData => {
+  return {
+    type: ADD_PRODUCT,
+    payload: {
+      productData
     }
+  }
 }
 
-export const delete_product = (productId) => {
-    return {
-        type: DELETE_PRODUCT,
-        payload: {
-            productId
-        }
+export const removeProduct = productId => {
+  return {
+    type: REMOVE_PRODUCT,
+    payload: {
+      productId
     }
+  }
 }
--- Lesson-3/hw1/src/cart.reducer.js
+++ Lesson-3/hw1/src/cart.reducer.js
@@ -1,22 +1,20 @@
-import { ADD_PRODUCT, DELETE_PRODUCT } from './cart.action';
+import { ADD_PRODUCT, REMOVE_PRODUCT } from './cart.actions'
 
 const initialState = {
-    productData: []
+  products: []
 }
 
 export const cartReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case ADD_PRODUCT:
-            return {
-                productData: state.productData.concat(action.payload.productData)
-            }
-        case DELETE_PRODUCT: {
-            const newData = 
-                state.productData.filter(({id}) => id !== action.payload.productId)
-            return {
-                ...state,
-                newData
-            }
-        }
-    }
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return {
+        products: state.products.concat(action.payload.productData)
+      }
+    case REMOVE_PRODUCT:
+      return {
+        products: state.products.filter(({id}) => id !== action.payload.productId)
+      }
+    default:
+      return state;
+  }
 }
