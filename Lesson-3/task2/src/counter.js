62bb38be7d4c685b2f5e3c743d8afaeae967d4eb
Shulha Nikita <shulgamalthael@gmail.com>
2020-11-12 16:40:09 +0200

    init files

--- Lesson-3/task2/src/counter.js
+++ Lesson-3/task2/src/counter.js
@@ -1,36 +1,32 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 export const INCREMENT = 'COUNTER/INCREMENT';
 export const DECREMENT = 'COUNTER/DECREMENT';
-export const RESET = 'COUNTER/RESET';
 
 export const increment = () => {
-    return {
-        type: INCREMENT
-    }
+  return {
+    type: INCREMENT
+  }
 }
 
 export const decrement = () => {
-    return {
-        type: DECREMENT
-    }
-}
-
-export const reset = () => {
-    return {
-        type: RESET
-    }
+  return {
+    type: DECREMENT
+  }
 }
 
 export const counterReducer = (state = 0, action) => {
-    switch(action.type) {
-        case INCREMENT:
-            return state + 1
-        case DECREMENT:
-            return state - 1
-        case RESET:
-            return state
-    }
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1
+    case DECREMENT:
+      return state - 1
+    default:
+      return state;
+  }
 }
 
-export const store = createStore(counterReducer);
+export const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
